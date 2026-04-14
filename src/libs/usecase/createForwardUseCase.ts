5a58e76a323ccb90a5ee1daba174477be018210a
sujay5372bot <sujay53721@gmail.com>
2026-04-14 11:56:17 +0530

    Update createForwardUseCase.ts

--- src/libs/usecase/createForwardUseCase.ts
+++ src/libs/usecase/createForwardUseCase.ts
@@ -17,7 +17,7 @@ async function createForwardUseCase(body, { repository, hasher }): Promise<{
             from: body.from,
             to: body.to,
             worker: body.worker,
-            hideSender: false
+            hideSender: boolean
         },
         message: `${body.name}, has been registerd`
     }
